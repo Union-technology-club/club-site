d394c9ca115547c0fbecc7edde4cd336c53170a0
joel <jgimbel@live.com>
2013-08-25 06:04:48 -0500

    compiled commit

---
+++ TTC/obj/Release/Package/PackageTmp/Scripts/js2.js
@@ -0,0 +1,12 @@
+﻿$("#shirt_selection").on("click", "input", function (e) {
+    var selection = e.currentTarget;
+    var sizes = $("#tshirt");
+    if (selection.value === "Yes") {
+        sizes.addClass("show");
+    } else {
+        sizes.removeClass("show");
+    }
+});
+$(".actions").click(function () {
+    $("#welcome").show(drop, 400);
+});
